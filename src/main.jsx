a016d5e88ac3c719c25c63076caed7d77e2d1e29
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2025-04-25 11:35:06 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, createRoutesFromElements, Route, BrowserRouter as Router, RouterProvider } from "react-router-dom";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+import App from "./App";
+import './index.css';
+import {Test} from './pages'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route path="" element={<Test/>} />
+    </Route>
+  )
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <ThirdwebProvider 
+      activeChain={Sepolia}
+      clientId="3f42e4634924ef9f347a9dc719674281"
+    >
+        <RouterProvider router={router}/>
+    </ThirdwebProvider>
+)
---
+++ src/pages/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='text-xl font-bold'>
+      Hello
+    </div>
+  )
+}
+
+export default Test
